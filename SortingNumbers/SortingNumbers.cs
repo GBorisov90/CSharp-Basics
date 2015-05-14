2a19fc9ff0a61f602fc7f446d7105a312263cba2
GBorisov90 <g.borisov123@gmail.com>
2015-05-14 12:59:43 +0300

    C# Basics

---
+++ SortingNumbers/SortingNumbers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+class SortingNumbers
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int[] numbers = new int[n];
+
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = int.Parse(Console.ReadLine());
+        }
+
+        Array.Sort(numbers);
+        foreach (var number in numbers)
+        {
+            Console.WriteLine(number);
+        }
+    }
+}
